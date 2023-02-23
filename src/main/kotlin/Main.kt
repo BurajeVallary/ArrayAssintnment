998c792fb912685abf7f6d9dabca6b83ec3afbd6
Vallary Buraje <vallaryburaje@gmail.com>
2023-02-23 08:18:02 +0300

    arrays added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,14 +10,13 @@ println(create.contentToString())
 }
 
 fun city(){
-    var vallary= arrayOf("harare","mumbai","dodoma","jakarta")
-    var gram= println(vallary.map { it.capitalize() })
+    var cities= arrayOf("harare","mumbai","dodoma","jakarta")
+    var gram= println(cities.map { it.capitalize() })
 
 }
 fun add(){
     var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
-    var sum = numbers[2] + numbers[5]
-    println(sum)
+    println(numbers.get(1)+numbers.get(4))
     println(numbers.indexOf(158))
     println(numbers.sortedArray().contentToString())
 }
